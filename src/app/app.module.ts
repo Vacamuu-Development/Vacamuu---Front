ae57e640cf85c264faf3e390db5d8b92b9b56cfb
ImChriis <christophermednabarroso@gmail.com>
2024-09-30 11:43:49 -0400

    Primeras configuraciones

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { HomeComponent } from './modules/admin/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
